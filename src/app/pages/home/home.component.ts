9a2457db694edb8a5e2011bf77bb269c1c5ff2aa
adisunjana <adisunjana443@gmail.com>
2025-01-21 12:42:01 +0700

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../components/hero/hero.component';
+import { PromotionBannerComponent } from '../../components/promotion-banner/promotion-banner.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    HeroComponent, PromotionBannerComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
