c0fbbb3de43b151eecf68e9a099202a34b7f0153
adisunjana <adisunjana443@gmail.com>
2025-01-21 11:43:29 +0700

    generate components and update app.config

---
+++ src/app/pages/produc-details/produc-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produc-details',
+  imports: [],
+  templateUrl: './produc-details.component.html',
+  styleUrl: './produc-details.component.scss'
+})
+export class ProducDetailsComponent {
+
+}
