f3eecdd21fed185e993ada15c422504d60c214ea
adisunjana <adisunjana443@gmail.com>
2025-01-22 14:42:42 +0700

    implement serach bar

--- src/app/pages/hotel/hotel.component.ts
+++ src/app/pages/hotel/hotel.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HotelCardComponent } from '../../components/hotel-card/hotel-card.component';
 import { AccordionFilterComponent } from '../../components/accordion-filter/accordion-filter.component';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-hotel',
   imports: [
     HotelCardComponent,
-    AccordionFilterComponent
+    AccordionFilterComponent,
+    SearchBarComponent
   ],
   templateUrl: './hotel.component.html',
   styleUrl: './hotel.component.scss'
